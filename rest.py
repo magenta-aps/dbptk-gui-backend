a4245c73ecb6e667e42d006bc6b106dfceae4823
Andreas Kring <andreas@magenta.dk>
2016-04-25 11:47:04 +0200

    Required and optional paramter lists added for all modules

--- rest.py
+++ rest.py
@@ -35,7 +35,7 @@ class Module(object):
             return {'status': STATUS.ERROR, 'message': MESSAGES.PARAMETER_LIST_EMPTY}
         for key in parameters:
             if not key in self.parameters:
-                return {'status': STATUS.ERROR, 'message': MESSAGES.INVALID_PARAMETER}
+                return {'status': STATUS.ERROR, 'message': MESSAGES.INVALID_PARAMETER, 'parameter': key}
         for key in self.parameters:
             if self.parameters[key] == PARAMETER.REQUIRED and key not in parameters:
                 return {'status': STATUS.ERROR, 'message': MESSAGES.REQUIRED_PARAMETER_MISSING, 'parameter': key}
@@ -46,9 +46,42 @@ class Module(object):
 
 
 class JSONChecker(object):
-    import_modules = {'jdbc': None, 'microsoft-access': None, 'microsoft-sql-server': None, 'mysql': Module({'import-hostname': PARAMETER.REQUIRED, 'import-database': PARAMETER.REQUIRED, 'import-username': PARAMETER.REQUIRED, 
-                              'import-password': PARAMETER.REQUIRED, 'import-port-number': PARAMETER.OPTIONAL}), 'oracle': None, 'postgresql': None, 'siard-1': None, 'siard-2': None, 'siard-dk': None}
-    export_modules = {'jdbc': None, 'list-tables': None, 'microsoft-sql-server': None, 'mysql': None, 'oracle': None, 'postgresql': None, 'siard-1': None, 'siard-2': None, 'siard-dk':Module({'export-folder': PARAMETER.REQUIRED, 'export-archiveIndex': PARAMETER.OPTIONAL, 'export-contextDocumentationIndex': PARAMETER.OPTIONAL, 'export-contextDocumentationFolder': PARAMETER.OPTIONAL})}
+    import_modules = {'jdbc': Module({'import-driver': PARAMETER.REQUIRED, 'import-connection': PARAMETER.REQUIRED}),
+                      'microsoft-access': Module({'microsoft-access': PARAMETER.REQUIRED}),
+                      'microsoft-sql-server': Module({'import-server-name': PARAMETER.REQUIRED, 'import-database': PARAMETER.REQUIRED,
+                                                     'import-username': PARAMETER.REQUIRED, 'import-password': PARAMETER.REQUIRED,
+                                                     'import-use-integrated-login': PARAMETER.OPTIONAL, 'import-disable-encryption': PARAMETER.OPTIONAL,
+                                                     'import-instance-name': PARAMETER.OPTIONAL, 'import-port-number': PARAMETER.OPTIONAL}),
+                      'mysql': Module({'import-hostname': PARAMETER.REQUIRED, 'import-database': PARAMETER.REQUIRED, 'import-username': PARAMETER.REQUIRED,
+                                        'import-password': PARAMETER.REQUIRED, 'import-port-number': PARAMETER.OPTIONAL}),
+                      'oracle': Module({'import-server-name': PARAMETER.REQUIRED, 'import-database': PARAMETER.REQUIRED, 'import-username': PARAMETER.REQUIRED,
+                                        'import-password': PARAMETER.REQUIRED, 'import-port-number': PARAMETER.REQUIRED, 'import-accept-license': PARAMETER.OPTIONAL}),
+                      'postgresql': Module({'import-hostname': PARAMETER.REQUIRED, 'import-database': PARAMETER.REQUIRED,
+                                            'import-username': PARAMETER.REQUIRED, 'import-password': PARAMETER.REQUIRED,
+                                            'import-disable-encryption': PARAMETER.OPTIONAL, 'import-port-number': PARAMETER.OPTIONAL}),
+                      'siard-1': Module({'import-file': PARAMETER.REQUIRED}),
+                      'siard-2': Module({'import-file': PARAMETER.REQUIRED}),
+                      'siard-dk': Module({'import-folder': PARAMETER.REQUIRED, 'import-as-schema': PARAMETER.REQUIRED})
+                      }
+    export_modules = {'jdbc': Module({'export-driver': PARAMETER.REQUIRED, 'export-connection': PARAMETER.REQUIRED}),
+                      'list-tables': Module({'export-file': PARAMETER.REQUIRED}),
+                      'microsoft-sql-server': Module({'export-server-name': PARAMETER.REQUIRED, 'export-database': PARAMETER.REQUIRED,
+                                                      'export-username': PARAMETER.REQUIRED, 'export-password': PARAMETER.REQUIRED,
+                                                      'export-use-integrated-login': PARAMETER.OPTIONAL, 'export-disable-encryption': PARAMETER.OPTIONAL,
+                                                      'export-instance-name': PARAMETER.OPTIONAL, 'export-port-number': PARAMETER.OPTIONAL}),
+                      'mysql': Module({'export-hostname': PARAMETER.REQUIRED, 'export-database': PARAMETER.REQUIRED, 'export-username': PARAMETER.REQUIRED,
+                                        'export-password': PARAMETER.REQUIRED, 'export-port-number': PARAMETER.OPTIONAL}),
+                      'oracle': Module({'export-server-name': PARAMETER.REQUIRED, 'export-database': PARAMETER.REQUIRED, 'export-username': PARAMETER.REQUIRED,
+                                        'export-password': PARAMETER.REQUIRED, 'export-port-number': PARAMETER.REQUIRED, 'export-accept-license': PARAMETER.OPTIONAL,
+                                        'export-source-schema': PARAMETER.OPTIONAL}),
+                      'postgresql': Module({'export-hostname': PARAMETER.REQUIRED, 'export-database': PARAMETER.REQUIRED,
+                                            'export-username': PARAMETER.REQUIRED, 'export-password': PARAMETER.REQUIRED,
+                                            'export-disable-encryption': PARAMETER.OPTIONAL, 'export-port-number': PARAMETER.OPTIONAL}),
+                      'siard-1': Module({'export-file': PARAMETER.REQUIRED, 'export-compress': PARAMETER.OPTIONAL, 'export-pretty-xml': PARAMETER.OPTIONAL,
+                                         'export-table-filter': PARAMETER.OPTIONAL}),
+                      'siard-2': Module({'export-file': PARAMETER.REQUIRED, 'export-compress': PARAMETER.OPTIONAL, 'export-pretty-xml': PARAMETER.OPTIONAL,
+                                         'export-table-filter': PARAMETER.OPTIONAL}),
+                      'siard-dk': Module({'export-folder': PARAMETER.REQUIRED, 'export-archiveIndex': PARAMETER.OPTIONAL, 'export-contextDocumentationIndex': PARAMETER.OPTIONAL, 'export-contextDocumentationFolder': PARAMETER.OPTIONAL})}
     
     @staticmethod
     def checkJson(json):
@@ -73,7 +106,15 @@ class JSONChecker(object):
         
         if resp['status'] == STATUS.ERROR:
             return resp
+
+        name = export_module['name']
+        parameters = export_module['parameters']
+        module = JSONChecker.export_modules[name]
+        resp = module.check_parameter_keys(parameters)
         
+        if resp['status'] == STATUS.ERROR:
+            return resp
+
         return {'status': message}
         
 # MySQL_import_module = Module({'import-hostname': PARAMETER.REQUIRED, 'import-database': PARAMETER.REQUIRED, 'import-username': PARAMETER.REQUIRED, 
