6db60053fc7eab90fe68ded814d327992d1c8694
Marie Crabit <mar.rie.cra@gmail.com>
2016-04-27 14:41:19 +0200

    fixing navigation

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,7 +1,21 @@
-// TODO: navigation, show the right tab
-// detect targeted tab
-// find header nav tag with same href?
-// switch active class to right nav tag
+$(".next").click(function(){
+	var that = this;
+	toggleNav(that);
+});
+
+$(".back").click(function(){
+	var that = this;
+	toggleNav(that);
+});
+function toggleNav(that) {
+	var tab = $(that).attr("href");
+	
+	var pill = $("[href='" + tab + "']").parent();
+	
+	$("header li").removeClass("active");
+	pill.addClass("active");
+}
+
 
 // TODO: deactivate not reached steps in nav
 
@@ -11,9 +25,37 @@ $(".coming-soon").click(function(){
 
 // TODO: Checkbox stuff: 
 // - select all
+$('#select-all').click(function() {   
+	$(':checkbox').each(function() {
+  	this.checked = true;
+	});
+});
 // - select none
+$('#select-none').click(function() {   
+	$(':checkbox').each(function() {
+  	this.checked = false;
+	});
+});
 // - invert selection
+$('#invert-selection').click(function() {
+	// TODO check first child if all siblings checked too
+	// TODO uncheck first child if 1 sibling unchecked	
+	$('table :not(:first-child) :checkbox').each(function() {
+		if(this.checked) {
+			this.checked = false;
+		} else {
+			this.checked = true;
+		}
+	});
+});
 // - select/deselect table
+$(".table-select-toggle").click(function(){
+	// TODO check first child if all siblings checked too
+	// TODO uncheck first child if 1 sibling unchecked	
+	// $(':checkbox').each(function() {
+  	// this.checked = true;
+	// });
+});
 
 
 // TODO: animate loading bar only when we get on this tab
@@ -26,7 +68,6 @@ function loadProgress() {
 		$(".loading-progress-status").text(loadingProgress + "%");
 	}
 };
-
 var intervalLoading = setInterval(loadProgress, 1000);
 
 
@@ -34,6 +75,7 @@ var intervalLoading = setInterval(loadProgress, 1000);
 // - "Connect & Next" should only forward when connection worked 
 // - animate loading bar according to process
 // - Generate content dynamically
+// - Make tables sortable
 
 // Others:
 // - Figure out what "Browse" does
