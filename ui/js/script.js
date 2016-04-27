5f2b556adb653c36086b4c229b570d8cbf81d9ad
Marie Crabit <mar.rie.cra@gmail.com>
2016-04-27 11:18:10 +0200

    initial layout stuff

---
+++ ui/js/script.js
@@ -0,0 +1,32 @@
+// TODO: navigation, show the right tab
+// detect targeted tab
+// find header nav tag with same href?
+// switch active class to right nav tag
+
+// TODO: deactivate not reached steps in nav
+
+// TODO: "coming up!" alert for sections: 
+// - contact us
+// - trouble shooting
+// - check details here
+
+$(".coming-soon").click(function(){
+	alert("Coming soon");
+});
+
+// TODO: Checkbox stuff: 
+// - select all
+// - select none
+// - invert selection
+// - select/deselect table
+
+// TODO later: (when database actually connected)
+// - "Connect & Next" should only forward when connection worked 
+// - animate loading bar according to process
+// - Generate content dynamically
+
+// TODO: animate loading bar
+
+// Others:
+// - Figure out what "Browse" does
+// - Figure out if dowload destination folder somehow choosable
