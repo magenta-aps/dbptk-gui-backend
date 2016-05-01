23a1ad21083c36f050e38033ed326f7930c0a463
frank <frank.thomsen@gmail.com>
2016-05-01 17:19:32 +0200

    added functionality to angular-based UI

--- angular-ui/main.js
+++ angular-ui/main.js
@@ -47,7 +47,60 @@ app.controller('DbptkCtrl', function($scope, $http) {
     });
     
     
-    // Commit the form and start importing
+    // Script for picking a folder from the filesystem
+    var pfdialog = document.querySelector('#pick-folder-dialog');
+    var showDialogButton = document.querySelector('#siard-dk-import-folder');
+    $scope.currentFolder = '/';
+    
+    if (! pfdialog.showModal) {
+        dialogPolyfill.registerDialog(pfdialog);
+    };
+    
+    showDialogButton.addEventListener('click', function() {
+        pfdialog.showModal();
+        $scope.traverseDown($scope.currentFolder);
+    });
+    
+    pfdialog.querySelector('.close').addEventListener('click', function() {
+        $scope.model.importModules['siard-dk']['import-folder'] = $scope.currentFolder;
+        $scope.currentFolder = '/';
+        pfdialog.close();
+    });
+    
+    $scope.traverseUp = function() {
+        
+    };
+    
+    $scope.traverseDown = function(currentPath) {
+        if (currentPath !== '/') {
+            $scope.currentFolder = $scope.currentFolder + '/' + currentPath;
+        };
+        $http.post( "http://localhost:5000/listdir", {'path': $scope.currentFolder} )
+        .then(function(response) {
+            // debugger;
+            $scope.folders = [];
+            for (item in response.data) {
+                if (response.data[item] === 'folder') {
+                    $scope.folders.push(item);
+                };
+            };
+        });
+    };
+    
+    $http.post( "http://localhost:5000/listdir", {'path': $scope.currentFolder} )
+    .then(function(response) {
+        // debugger;
+        $scope.folders = [];
+        for (item in response.data) {
+            if (response.data[item] === 'folder') {
+                console.log(item);
+                $scope.folders.push(item);
+            };
+        };
+    });
+    
+    
+    // Commit the form and start exporting
     $scope.startImport = function() {
         
         $scope.importData = {
@@ -77,12 +130,55 @@ app.controller('DbptkCtrl', function($scope, $http) {
             $scope.logger = response.data;
         });
         
+        // Get status updates while exporting
+        $http.get( "http://localhost:5000/status" )
+        .then(function(response) {
+            // debugger;
+            $scope.logger = response.data;
+            if (response.data.status === 'NOT RUNNING') {
+                $scope.exportStatus = response.data.status;
+                document.querySelector('#export-progress').addEventListener('mdl-componentupgraded', function() {
+                    this.MaterialProgress.setProgress(0);
+                });
+            } else if (response.data.status === 'RUNNING') {
+                $scope.exportStatus = response.data.status;
+            } else if (response.data.status === 'DONE') {
+                $scope.exportStatus = response.data.status;
+                document.querySelector('#export-progress').addEventListener('mdl-componentupgraded', function() {
+                    this.MaterialProgress.setProgress(100);
+                });
+            } else {
+                $scope.exportStatus = 'Error ' + response.data.status;
+                document.querySelector('#export-progress').addEventListener('mdl-componentupgraded', function() {
+                    this.MaterialProgress.setProgress(0);
+                });
+            }
+            
+        });
+        
+    };
+    
+    
+    // Cancel ongoing export
+    $scope.cancelExport = function() {
+        
+        // Send termination request
+        $http.get( "http://localhost:5000/terminate" )
+        .then(function(response) {
+            // debugger;
+            $scope.logger = response.data;
+        });
+        
+        // Navigate to home screen;
+        $scope.states.progress = false;
+        $scope.states.home = true;
+        
     };
     
     
     // A little navigation scripting
     $scope.states = {
-        home: true,
+        home: true, // Default is displaying home
         connect: false,
         output: false,
         progress: false
